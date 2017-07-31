77a4aa9f9332444ea0222eea8bee27ddcb0a8eb9
Charles Renk <renkx008@umn.edu>
2017-07-31 14:33:52 -0500

    tried to make the swap work, but to no avail

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ $(document).ready(function(){
     var clicks = 0
     $('#generateButton').on('click', function(){
         clicks ++
-        $('body').append('<div><p> You have clicked this button ' + 
-        clicks + ' times.</p>' + 
+        $('body').append('<div><p>' + 
+        clicks + '</p>' + 
         '<button id = "swapButton">Swap</button> <button id = "deleteButton">Delete</button>' +
         '</div>')
         $('div').css('background-color', 'red')
     })
     $('body').on('click', '#deleteButton', function(){
         $(this).parent().remove();
-});
+    });
+    $('body').on('click', '#swapButton', function(){
+       
+    });
    
 });
