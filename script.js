ea84f7a17f8a9dbcd71cac661f789792f853d9d7
Charles Renk <renkx008@umn.edu>
2017-07-31 13:50:10 -0500

    added swap and delete buttons when generate is clicked and also changed each newly created div background-clolor to red

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
     var clicks = 0
     $('#generateButton').on('click', function(){
         clicks ++
-        $('body').append('<div>' +
-        '<p>You have clicked this button ' + 
-        clicks + ' times.</p></div>')
+        $('body').append('<div></div>')
+        $('div').append('<p> You have clicked this button ' + 
+        clicks + ' times.</p>')
+        $('div').append('<button id = "swapButton">Swap</button><button id = "deleteButton">Delete</button>')
+        $('div').css('background-color', 'red')
     })
 });
